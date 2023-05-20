51756854ff157440aae95ea2fb938b6a389e64ee
Rakibul Hasan <rakibulhasan6733@gmail.com>
2023-05-21 00:23:49 +0600

    Adde Featured Profiles in Real Mate website

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+$(document).on('ready', function() {
+  $(".center").slick({
+    dots: false,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
