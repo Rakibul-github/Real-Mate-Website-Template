29766b9cedf76d22e747aac2d66fa1da5a6ea344
Rakibul Hasan <rakibulhasan6733@gmail.com>
2023-05-23 22:06:14 +0600

    Added testimonial section in Real Mate Website template

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,11 +21,48 @@ $(document).on('ready', function() {
         breakpoint: 767,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+// testimonial carrosel 
+$(document).on('ready', function() {
+  $(".testimonialCarrosel").slick({
+    dots: true,
+    infinite: true,
+    centerMode: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
